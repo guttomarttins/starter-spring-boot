1b13f8d1a4b74bffe371b2aeda14a6d76fcda4ce
Carlos Martins <guttomarttins@gmail.com>
2019-02-17 17:05:11 -0300

    Iniciando projeto

---
+++ starter-spring-boot/src/main/java/br/com/guttomarttins/ssb/StarterSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.guttomarttins.ssb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterSpringBootApplication.class, args);
+	}
+
+}
